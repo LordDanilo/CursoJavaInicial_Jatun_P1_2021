8f6bfa3b315778c168abb94749a4046c88eda048
LordDanilo <64174678+LordDanilo@users.noreply.github.com>
2021-10-16 11:05:07 -0400

    Ejemplos

---
+++ Extras/Ejemplo.java
@@ -0,0 +1,17 @@
+public class Ejemplo{
+    final static double gravedad = 9.8;
+    int edad;
+
+    public Ejemplo(int valor){
+        edad = valor;
+    }
+    public double calcular(){
+        return edad+gravedad;
+    }
+
+}
+//modificador de acceso 
+//expresion 
+//variable 
+//tipo de dato 
+//sentencia de control condicional
---
+++ Extras/OtroEjemplo.java
@@ -0,0 +1,11 @@
+public class OtroEjemplo{
+    //Estructuras de Datos
+    public boolean verificarNumero(int numero){
+        int edad = 19;
+    }
+}
+//modificador de acceso
+//tipo de retorno:
+//parametro
+//variable
+//tipo de Dato
